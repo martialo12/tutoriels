9fc9efc75b3bbcfe6317d41e9258a71eb75b068d
martialo12 <martial.wafo@kwantis.com>
2018-12-22 21:42:32 +0100

    refactoring code

--- app.py
+++ app.py
@@ -53,6 +53,22 @@ def hello():
     return render_template('hello.html')
 
 
+# using jinja template in html
+@app.route('/films')
+def films():
+    films_dict = {
+        'BLACK PANTHER': 2.5,
+        'Avengers: Infinity War': 3.2,
+        'Ready Player One': 2.14,
+        'Les Indestructibles 2': 1.48,
+        'Woman at War': 2.52,
+        'The Guilty': 1.5,
+        'Deadpool 2': 3.5,
+        'Mission Impossible - Fallout': 1.7
+    }
+    return render_template('table_movies.html', films=films_dict, name='Sorelle')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
