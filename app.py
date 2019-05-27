69d218ec7d5007507a5cdd59d54cc3396139a255
martialo12 <martialo218@gmail.com>
2019-05-27 21:10:58 +0200

    books table added

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,7 +8,7 @@ app = Flask(__name__)
 app.config.update(
 
     SECRET_KEY='topsecret',
-    SQLALCHEMY_DATABASE_URI='postgresql://postgres:Seigneur1@localhost/catalog',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:password@localhost/catalog',
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
@@ -101,12 +103,41 @@ class Publication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return 'The id is {}, Name is is {}'.format(self.id, self.name)
+        return ' Name  is {}'.format(self.name)
+
+
+# BOOK TABLE
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # ESTABLISH A RELATIONSHIP BETWEEN PUBLICATION AND BOOK TABLES
+    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+    def __init__(self, title, author, avg_rating, book_format, image, num_pages, pub_id):
+
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.format = book_format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+    def __repr__(self):
+        return 'title of book is {} by {}'.format(self.title, self.author)
 
 
 if __name__ == '__main__':
